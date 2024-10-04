cae113099339d2fb1d6258593f07ae8c97c3b6d0
jayramani96 <jayramani100@gamail.com>
2024-10-04 16:53:26 +0530

    commited

---
+++ 04]Class and Function Component/src/Component/Class.jsx
@@ -0,0 +1,19 @@
+import './Class.css'
+import React from 'react'
+
+export default function Class() {
+  return (
+    <div className='main'>
+        <nav>
+            <link rel="stylesheet" href="" />
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Services</a>
+            <a href="">Blog</a>
+            <a href="">Login</a>
+        </nav>
+
+        <h1>Class And Function Components</h1>
+    </div>
+  )
+}
