fc80c760750e8a188055cfe6d32d73f41758d9bb
jayramani96 <jayramani100@gamail.com>
2024-10-08 16:45:36 +0530

    commited

---
+++ 06]PROJECT-01/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './Component/ClassCounter'
+import ClassCounter from './Component/ClassCounter'
+
+function App() {
+
+  return (
+    <>
+    <ClassCounter/>
+    </>
+  )
+}
+
+export default App
