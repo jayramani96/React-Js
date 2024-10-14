9c47eb4ba76a088e50feca45d41ddd1ecf69e2e0
jayramani96 <jayramani100@gamail.com>
2024-10-14 15:19:43 +0530

    commited

--- 09]Project-02/src/Pr-useEffect/Pr_UseEffect.jsx
+++ 09]Project-02/src/Pr-useEffect/Pr_UseEffect.jsx
@@ -5,15 +5,16 @@ export default function Pr_UseEffect() {
 
     useEffect(()=>{
         
-        return console.log('mounting');
+       console.log('mounting');
     },[num])
   return (
     <div className='M'>
         <h1>Pr_UseEffect</h1>
         <h1>{num}</h1>
         <button onClick={()=>setNum(num-1)}>-</button>
-        <button onClick={()=>setNum(num+1)}>+</button>
         <button onClick={()=>setNum(0)}><i className="fa-solid fa-arrow-rotate-right"></i></button>
+        <button onClick={()=>setNum(num+1)}>+</button>
+        
     </div>
   )
 }
