3f9ca7b5d4628da7adfe92a0389a2ce5c6fa9478
jayramani96 <jayramani100@gamail.com>
2024-10-23 16:29:52 +0530

    commited

---
+++ 17]ReactBootStrap_Setup/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <Button className='btn btn-danger'>
+        Click
+      </Button>
+    </>
+  )
+}
+
+export default App
